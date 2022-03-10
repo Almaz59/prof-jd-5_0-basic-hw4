28da3a9747139ad254a10c33e398fbe4b5f811ce
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-03-10 16:35:46 +0300

    hw 4 обязательные задачи

--- src/ru/stepik/Main.java
+++ src/ru/stepik/Main.java
@@ -3,6 +3,31 @@ package ru.stepik;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Задача 1:
+        int a = 1;
+        while (a <= 10) {
+            System.out.print(a + " ");
+            a++;
+        }
+        System.out.println("");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        // Задача 2:
+        int friday = 5;
+        for (; friday <= 31; friday += 7) {
+            System.out.println("Сегодня пятница " + friday + "-е число");
+        }
+
+        // Задача 3:
+        int pastYear = 1822;
+        int futureYear = 2122;
+        for (int i = 0; i < futureYear; i += 79) {
+            if (i >= pastYear)
+                System.out.println(i + " ");
+        }
     }
 }
